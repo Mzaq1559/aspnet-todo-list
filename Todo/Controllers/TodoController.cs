f5b8569f80f231c0031f4d77cb09ff80e8643686
Mzaq1559 <muhammadzulqarnain1559@gmail.com>
2025-12-24 14:22:03 +0500

    first

---
+++ Todo/Controllers/TodoController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers;
+
+public class TodosController : Controller
+{
+    private static List<TodoItem> _todos = new()
+    {
+        new TodoItem { Id = 1, Title = "Learn ASP.NET Core", IsCompleted = false },
+        new TodoItem { Id = 2, Title = "Build a Todo MVC App", IsCompleted = true }
+    };
+
+    private static int _nextId = 3;
+
+    // GET: /Todos or /Todos/Index
+    public IActionResult Index()
+    {
+        return View(_todos);
+    }
+
+    // GET: /Todos/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: /Todos/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(TodoItem todoItem)
+    {
+        if (ModelState.IsValid)
+        {
+            todoItem.Id = _nextId++;
+            todoItem.CreatedAt = DateTime.UtcNow;
+            _todos.Add(todoItem);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(todoItem);
+    }
+
+    // GET: /Todos/Edit/5
+    public IActionResult Edit(int id)
+    {
+        var todo = _todos.FirstOrDefault(t => t.Id == id);
+        if (todo == null)
+            return NotFound();
+
+        return View(todo);
+    }
+
+    // POST: /Todos/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, TodoItem todoItem)
+    {
+        if (id != todoItem.Id)
+            return BadRequest();
+
+        if (ModelState.IsValid)
+        {
+            var existing = _todos.FirstOrDefault(t => t.Id == id);
+            if (existing == null)
+                return NotFound();
+
+            existing.Title = todoItem.Title;
+            existing.IsCompleted = todoItem.IsCompleted;
+
+            return RedirectToAction(nameof(Index));
+        }
+        return View(todoItem);
+    }
+
+    // GET: /Todos/Delete/5
+    public IActionResult Delete(int id)
+    {
+        var todo = _todos.FirstOrDefault(t => t.Id == id);
+        if (todo == null)
+            return NotFound();
+
+        return View(todo);
+    }
+
+    // POST: /Todos/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var todo = _todos.FirstOrDefault(t => t.Id == id);
+        if (todo != null)
+            _todos.Remove(todo);
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Todo/Models/TodoItem.cs.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
